d4c7ef1a9f61db72a60151a510f4c4c1ac7814e6
David Sanchez <dsmdavid@gmail.com>
2022-06-09 21:48:13 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,3 +29,12 @@ fr_response = requests.get("https://fruityvice.com/api/fruit/" + str(fruit_choic
 
 fr_norm = pd.json_normalize(fr_response.json())
 st.dataframe(fr_norm)
+
+
+
+my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+st.text("Hello from Snowflake:")
+st.text(my_data_row)
