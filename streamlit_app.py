37cad621ab9c315e0e1e28dc5aca15e2efabe64a
David Sanchez <dsmdavid@gmail.com>
2022-06-09 15:54:39 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ import streamlit as st
 st.title("Let's open a Diner")
 
 st.header("What's on the menu?")
-st.text("Sausages, bacon and 3 eggs --sunny side up")
-st.text("Kale")
+st.text("🥣 🥗 🐔 🍞 Sausages, bacon and 3 eggs --sunny side up")
+st.text("Kale and 🥑")
 
